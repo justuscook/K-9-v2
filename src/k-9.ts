b77206b5db7c807c4b2a79c3102f0d84ba2438fe
justuscook <justus_cook@yahoo.com>
2022-05-13 14:45:43 -0500

    more comments

--- src/k-9.ts
+++ src/k-9.ts
@@ -8,7 +8,7 @@ import cors from 'cors';
 import axois from 'axios'
 import { getAuthToken, getSpreadSheetValues, ISheetData, ISpreadSheetValues, leaderbaordSheetId, mapValuesFromValues } from './utils/sheets';
 
-
+//Info used to load commands into bot and register them to discrod, also has info for help command later
 export interface ICommandInfo {
     name: string,
     execute: any,
@@ -16,35 +16,47 @@ export interface ICommandInfo {
     data: any
 }
 
+//start express server for api
 const app = express();
+//user cors, think its needed for google apis
 app.use(cors());
+//use json body format
 app.use(express.json());
+//listen on port 9002 http
 app.listen(9002, () => {
     console.log('Listening on port 9002!')
 })
 
+//listen for data at :9002/leaderboard
 app.post('/leaderboard', async (req: Request, res: Response) => {
+    //the data sent by the api when a cell is edited, in the google sheets Extensions, appScript
     const reqData: ISheetData = req.body;
     console.log(reqData);
+    //Return that info was recieved successfully
     res.status(200).send('Data received from google sheets!');
+    //get the values from the sheet, currently hard coded for testing
     const data: ISpreadSheetValues = await getSpreadSheetValues({
         sheetNameOrRange: `${reqData.sheetName}!R${1}C${1}:R${12}C${5}`,
         auth: await getAuthToken(),
         spreadsheetId: leaderbaordSheetId
     });
+    //Get the data in a form to use
     const messageData = mapValuesFromValues(data);
+    //get the discord channel update the leaderboard in, might need to send this info from google. Hardcoded to #champ-build-bot channel atm
     const channel = (await (await client.guilds.fetch('514616202249895936')).channels.fetch('620344940852936714') as GuildTextBasedChannel);
     console.log(messageData)
+    //Get the leader board message by ID
     const leaderboardMessage = await channel.messages.fetch(messageData.MessageId);
+    //Get value of cunnent embed
     let embed = leaderboardMessage.embeds[0];
+    //Set the description of the embed to the values of the sheet
     embed.setDescription(messageData.data.join('\n'));
-    leaderboardMessage.edit({embeds: [embed]})
-
-    
+    //etit the message
+    await leaderboardMessage.edit({ embeds: [embed] })
 });
 
 const commands: ICommandInfo[] = [];
-
+//discord client settings needed to get the api responses we need
 export const client: Client = new Client({
     intents:
         [
@@ -57,6 +69,8 @@ export const client: Client = new Client({
     ]
 });
 
+
+//register commands with discord
 async function deployCommands() {
 
     const commandFiles = fs.readdirSync(__dirname + '/commands').filter(file => file.endsWith('.js'));
@@ -83,21 +97,27 @@ async function deployCommands() {
         }
     })();
 }
-
+//Bog bot into discord
 client.login(token);
+//when successfull, update the commands
 client.once('ready', async () => {
     await deployCommands();
 })
 
+
+//Interaction command handler
 client.on('interactionCreate', async (interaction: Interaction) => {
+    //if its not a command interaction ignore it, for now...
     if (!interaction.isCommand()) return;
-
+    //Get the command name, and look for the command file
     const command: ICommandInfo | undefined = commands.find(x => x.name === interaction.commandName);
-
+    //if the command isnt found, do nothing
     if (!command) return;
+    //else execute the found command
     else {
         const commandSuccess: Promise<boolean> = await command.execute(interaction);
     }
+    //Console output so you know bot connected
     console.log('K-9 Online!')
 })
 
--- src/utils/general.ts
+++ src/utils/general.ts
@@ -1,5 +1,10 @@
 import { Message } from "discord.js";
 
+/**
+ * Deletes a message or group of messages after a given time
+ * @param messages Array of messges to delete
+ * @param time Time in ms
+ */
 export async function delayDelete(messages: Message[], time: number = 10000) {
     setTimeout(async () => {
         for (const m of messages) {
